f72f812b407e32ed506816ef5fa7efe1dd631754
Syeda Umme Ayman <aymanshoity@gmail.com>
2025-12-06 00:33:26 +0600

    vehicle crud completed

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,26 +1,33 @@
 import config from "."
-import {Pool} from 'pg'
-export const pool=new Pool({
-   connectionString:`${config.connectionString}`
+import { Pool } from 'pg'
+export const pool = new Pool({
+   connectionString: `${config.connectionString}`
 })
 
-export const initDB=async()=>{
+export const initDB = async () => {
+   // await pool.query(`CREATE TYPE role_enum AS ENUM('admin', 'customer')`)
    await pool.query(`CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
       email VARCHAR(150) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      role VARCHAR(200) NOT NULL,
-      phone VARCHAR(15) NOT NULL,
-      CHECK (char_length(password)>=6 )
+      password VARCHAR(255) NOT NULL CHECK (char_length(password)>=6 ),
+      role VARCHAR(150) NOT NULL,
+      phone VARCHAR(15) NOT NULL
+      )`)
+
+   // await pool.query(`CREATE TYPE vehicle_type_enum AS ENUM('car', 'bike', 'van', 'SUV')`)
+   // await pool.query(`CREATE TYPE availability_status_enum AS ENUM('available', 'booked')`)
+   await pool.query(`
+      CREATE TABLE IF NOT EXISTS vehicles (
+      id SERIAL PRIMARY KEY,
+      vehicle_name VARCHAR(100) NOT NULL,
+      type VARCHAR(50) NULL,
+      registration_number VARCHAR(50) UNIQUE NOT NULL,
+      daily_rent_price NUMERIC(10,2) NOT NULL ,
+      availability_status VARCHAR(50) NOT NULL,
+      CHECK (daily_rent_price>=0)
       )`)
 
 
-   // await pool.query(`CREATE TABLE IF NOT EXISTS cars (
-   //    id SERIAL PRIMARY KEY,
-   //    make VARCHAR(50) NOT NULL,
-   //    model VARCHAR(50) NOT NULL,
-   //    year INT NOT NULL,
-   //    price DECIMAL(10, 2) NOT NULL
-   //    )`)
+
 }
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ initDB()
 app.get('/', (req: Request, res: Response) => {
    res.send('Vrooooooooooom!!!! carrr')
 })
+// User Crud Api
 app.post('/api/v1/auth/signup', async (req: Request, res: Response) => {
    console.log(req.body)
    const { name, email, password, role, phone } = req.body
@@ -52,10 +53,10 @@ app.get('/api/v1/users', async (req: Request, res: Response) => {
    }
 
 })
-app.get('/api/v1/users/:id', async (req: Request, res: Response) => {
+app.get('/api/v1/users/:userId', async (req: Request, res: Response) => {
 
    try {
-      const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [req.params.id])
+      const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [req.params.userId])
       if (result.rows.length === 0) {
          res.status(404).json({
             success: false,
@@ -65,6 +66,103 @@ app.get('/api/v1/users/:id', async (req: Request, res: Response) => {
          res.status(200).json({
             success: true,
             message: 'Users retrieved successfully',
+            data: result.rows[0]
+
+         })
+      }
+
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error' })
+   }
+
+})
+app.put('/api/v1/users/:userId', async (req: Request, res: Response) => {
+   const { name, email, role, phone } = req.body
+   try {
+      const result = await pool.query(`UPDATE users SET name=$1, email=$2,  role=$3, phone=$4 WHERE id=$5 RETURNING *`, [name, email, role, phone, req.params.userId])
+      console.log(result)
+      if (result.rowCount === 0) {
+         res.status(404).json({
+            success: false,
+            message: "user not found"
+         })
+      } else {
+
+         res.status(200).json({
+            success: true,
+            message: 'User updated successfully',
+            data: result.rows[0]
+
+         })
+      }
+
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error', details: err.message })
+   }
+
+})
+app.delete('/api/v1/users/:userId', async (req: Request, res: Response) => {
+
+   try {
+      const result = await pool.query(`DELETE FROM users WHERE id=$1`, [req.params.userId])
+      console.log(result)
+      if (result.rowCount === 0) {
+         res.status(404).json({
+            success: false,
+            message: "user not found"
+         })
+      } else {
+
+         res.status(200).json({
+            success: true,
+            message: 'User deleted successfully',
+            data: result.rows
+
+         })
+      }
+
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error', details: err.message })
+   }
+
+})
+// Vehicle Crud Api
+app.post('/api/v1/vehicles', async (req: Request, res: Response) => {
+   console.log(req.body)
+   const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body
+
+   try {
+      const result = await pool.query(`INSERT INTO vehicles (vehicle_name,type,registration_number,daily_rent_price,availability_status) VALUES ($1, $2 ,$3 ,$4, $5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status])
+      console.log(result.rows[0])
+      res.status(201).json({
+         success: true,
+         message: 'Vehicle created successfully',
+         data: result.rows[0]
+
+      })
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error', details: err.message })
+   }
+
+})
+app.get('/api/v1/vehicles', async (req: Request, res: Response) => {
+
+   try {
+      const result = await pool.query(`SELECT * FROM vehicles`)
+      if (result.rows.length === 0) {
+         res.status(404).json({
+            success: true,
+            message: "Vehicle not found",
+            data: result.rows
+         })
+      } else {
+         res.status(200).json({
+            success: true,
+            message: 'Vehicles retrieved successfully',
             data: result.rows
 
          })
@@ -76,21 +174,48 @@ app.get('/api/v1/users/:id', async (req: Request, res: Response) => {
    }
 
 })
-app.put('/api/v1/users/:id', async (req: Request, res: Response) => {
-   const { name, email, role, phone } = req.body
+app.get('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) => {
+
    try {
-      const result = await pool.query(`UPDATE users SET name=$1, email=$2,  role=$3, phone=$4 WHERE id=$5 RETURNING *`, [name, email, role, phone, req.params.id])
-      console.log(result)
-      if (result.rowCount === 0) {
+      const result = await pool.query(`SELECT * FROM vehicles WHERE id=$1`, [req.params.vehicleId])
+      if (result.rows.length === 0) {
          res.status(404).json({
-            success: false,
-            message: "user not found"
+            success: true,
+            message: "Vehicle not found",
+            data: result.rows
          })
       } else {
-       
          res.status(200).json({
             success: true,
-            message: 'User updated successfully',
+            message: 'Vehicle retrieved successfully',
+            data: result.rows[0]
+
+         })
+      }
+
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error' })
+   }
+
+})
+app.put('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) => {
+   // console.log(req.body)
+   const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body
+   try {
+      const result = await pool.query(`UPDATE vehicles SET vehicle_name=$1, type=$2,  registration_number=$3, daily_rent_price=$4, availability_status=$5 WHERE id=$6 RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status, req.params.vehicleId])
+      // console.log(result)
+      if (result.rowCount === 0) {
+         res.status(404).json({
+            success: true,
+            message: "Vehicle not found",
+            data: result.rows
+         })
+      } else {
+
+         res.status(200).json({
+            success: true,
+            message: 'Vehicle updated successfully',
             data: result.rows[0]
 
          })
@@ -102,21 +227,22 @@ app.put('/api/v1/users/:id', async (req: Request, res: Response) => {
    }
 
 })
-app.delete('/api/v1/users/:id', async (req: Request, res: Response) => {
-  
+app.delete('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) => {
+
    try {
-      const result =await pool.query(`DELETE FROM users WHERE id=$1`, [req.params.id])
+      const result =await pool.query(`DELETE FROM vehicles WHERE id=$1`, [req.params.vehicleId])
       console.log(result)
       if (result.rowCount === 0) {
          res.status(404).json({
-            success: false,
-            message: "user not found"
+            success: true,
+            message: "Vehicle not found",
+            data: result.rows
          })
       } else {
-       
+
          res.status(200).json({
             success: true,
-            message: 'User deleted successfully',
+            message: 'Vehicle deleted successfully',
             data: result.rows
 
          })
