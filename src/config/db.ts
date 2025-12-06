9b7859d1ccc9c23db7674202f55742346022123b
Syeda Umme Ayman <aymanshoity@gmail.com>
2025-12-06 14:21:12 +0600

    booking apis getting ready

--- src/config/db.ts
+++ src/config/db.ts
@@ -23,10 +23,21 @@ export const initDB = async () => {
       vehicle_name VARCHAR(100) NOT NULL,
       type VARCHAR(50) NULL,
       registration_number VARCHAR(50) UNIQUE NOT NULL,
-      daily_rent_price NUMERIC(10,2) NOT NULL ,
+      daily_rent_price INT NOT NULL ,
       availability_status VARCHAR(50) NOT NULL,
       CHECK (daily_rent_price>=0)
       )`)
+
+   await pool.query(`
+      CREATE TABLE IF NOT EXISTS bookings(
+      id SERIAL PRIMARY KEY,
+      customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+      vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+      rent_start_date DATE NOT NULL,
+      rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+      total_price INT NOT NULL ,
+      status VARCHAR(50) NOT NULL
+   )`)
 
 
 
--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ initDB()
 app.get('/', (req: Request, res: Response) => {
    res.send('Vrooooooooooom!!!! carrr')
 })
-// User Crud Api
+// auth crud api
 app.post('/api/v1/auth/signup', async (req: Request, res: Response) => {
    console.log(req.body)
    const { name, email, password, role, phone } = req.body
@@ -37,6 +37,7 @@ app.post('/api/v1/auth/signup', async (req: Request, res: Response) => {
    }
 
 })
+// User Crud Api
 app.get('/api/v1/users', async (req: Request, res: Response) => {
 
    try {
@@ -99,7 +100,7 @@ app.put('/api/v1/users/:userId', async (req: Request, res: Response) => {
 
 
    } catch (err: any) {
-      return res.status(500).json({ message: 'Internal server error', details: err.message })
+      return res.status(500).json({ success: false, message: 'Internal server error', details: err.message })
    }
 
 })
@@ -125,7 +126,7 @@ app.delete('/api/v1/users/:userId', async (req: Request, res: Response) => {
 
 
    } catch (err: any) {
-      return res.status(500).json({ message: 'Internal server error', details: err.message })
+      return res.status(500).json({ success: false, message: 'Internal server error', details: err.message })
    }
 
 })
@@ -145,7 +146,7 @@ app.post('/api/v1/vehicles', async (req: Request, res: Response) => {
       })
 
    } catch (err: any) {
-      return res.status(500).json({ message: 'Internal server error', details: err.message })
+      return res.status(500).json({ success: false, message: 'Internal server error', details: err.message })
    }
 
 })
@@ -223,14 +224,14 @@ app.put('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) => {
 
 
    } catch (err: any) {
-      return res.status(500).json({ message: 'Internal server error',details:err.message })
+      return res.status(500).json({ message: 'Internal server error', details: err.message })
    }
 
 })
 app.delete('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) => {
 
    try {
-      const result =await pool.query(`DELETE FROM vehicles WHERE id=$1`, [req.params.vehicleId])
+      const result = await pool.query(`DELETE FROM vehicles WHERE id=$1`, [req.params.vehicleId])
       console.log(result)
       if (result.rowCount === 0) {
          res.status(404).json({
@@ -250,7 +251,110 @@ app.delete('/api/v1/vehicles/:vehicleId', async (req: Request, res: Response) =>
 
 
    } catch (err: any) {
-      return res.status(500).json({ message: 'Internal server error',details:err.message })
+      return res.status(500).json({ message: 'Internal server error', details: err.message })
+   }
+
+})
+
+// bookings Crud Api
+app.post('/api/v1/bookings', async (req: Request, res: Response) => {
+   // console.log(req.body)
+   const { customer_id, vehicle_id, rent_start_date, rent_end_date } = req.body
+
+   // const getUser = await pool.query(`SELECT * FROM users WHERE id=$1 `, [customer_id])
+   const getVehicle = await pool.query(`SELECT * FROM vehicles WHERE id=$1 `, [vehicle_id])
+   // console.log(getUser.rows[0])
+   // console.log(getVehicle.rows[0])
+   try {
+      if (getVehicle.rows[0].availability_status !== 'available') {
+         return res.status(400).json({
+            success: false,
+            message: 'Vehicle is not available for booking'
+         })
+      } else {
+         const start: Date = new Date(rent_start_date)
+         const end: Date = new Date(rent_end_date)
+         const total_rent_days: number = (end.getTime() - start.getTime()) / (1000 * 3600 * 24)
+         const total_price: number = Number(total_rent_days * Number(getVehicle.rows[0].daily_rent_price))
+         const status = 'active'
+         const vehicle_availability_status = 'booked'
+         const vehicle_object = {
+            vehicle: {
+               vehicle_name: getVehicle.rows[0].vehicle_name,
+               daily_rent_price: getVehicle.rows[0].daily_rent_price
+            }
+         }
+
+         // insert data into bookings table
+         const result = await pool.query(`INSERT INTO bookings (customer_id, vehicle_id,rent_start_date,rent_end_date,total_price,status) VALUES ($1, $2 ,$3 ,$4, $5 ,$6) RETURNING *`, [customer_id, vehicle_id, rent_start_date, rent_end_date, total_price as number, status])
+         console.log(result.rows[0])
+         if (result.rows[0]) {
+            // update vehicle availability status
+            await pool.query(`UPDATE vehicles SET availability_status=$1 WHERE id=$2`, [vehicle_availability_status, vehicle_id])
+            res.status(201).json({
+               success: true,
+               message: 'Booking created successfully',
+               data: { ...result.rows[0], ...vehicle_object }
+
+            })
+         }
+
+      }
+
+
+
+
+   } catch (err: any) {
+      return res.status(500).json({ success: false, message: 'Internal server error', details: err.message })
+
+   }
+
+})
+app.get('/api/v1/bookings', async (req: Request, res: Response) => {
+
+   try {
+      const result = await pool.query(`SELECT * FROM bookings`)
+      if (result.rows.length === 0) {
+         res.status(404).json({
+            success: true,
+            message: "Vehicle not found",
+            data: result.rows
+         })
+      } else {
+         const getUser = await pool.query(`SELECT * FROM users WHERE id=$1 `, [result.rows[0].customer_id])
+         const getVehicle = await pool.query(`SELECT * FROM vehicles WHERE id=$1 `, [result.rows[0].vehicle_id])
+         const vehicle_object = {
+            vehicle: {
+               vehicle_name: getVehicle.rows[0].vehicle_name,
+               daily_rent_price: getVehicle.rows[0].daily_rent_price,
+               type: getVehicle.rows[0].type
+            }
+         }
+         const admin_view = {
+            customer: {
+               name: getUser.rows[0].name,
+               email: getUser.rows[0].email
+            },
+             vehicle: {
+               vehicle_name: getVehicle.rows[0].vehicle_name,
+               daily_rent_price: getVehicle.rows[0].daily_rent_price
+            }
+         }
+         res.status(200).json({
+            success: true,
+            message: 'Vehicles retrieved successfully',
+            data: result.rows.map(booking=>{
+               return {
+                  ...booking,...admin_view
+               }
+            })
+
+         })
+      }
+
+
+   } catch (err: any) {
+      return res.status(500).json({ message: 'Internal server error' })
    }
 
 })
